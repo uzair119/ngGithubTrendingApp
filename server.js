422bb87713504333f58299086f00b1dc53b65716
Uzair Ahmed <uzairahmed11d@gmail.com>
2019-08-01 16:54:53 +0500

    heroku deployment

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/ngGithubTrendingApp'));
+app.use(express.static(__dirname + '/src/app'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ngGithubTrendingApp/index.html'));
+  res.sendFile(path.join(__dirname + '/src/app/app.component.html'));
 });
 
 // default Heroku PORT
